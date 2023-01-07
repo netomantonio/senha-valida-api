5dfa5b37c931ed56f39b90a9036f0a21dcf2b0fe
[ N E T O ] Antonio Miranda <netomantonio@gmail.com>
2023-01-07 03:18:38 -0400

    projeto inicial, gradle, kotlin, springboot e graphql-kotlin

---
+++ build.gradle.kts
@@ -0,0 +1,36 @@
+import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
+
+plugins {
+	id("org.springframework.boot") version "2.7.7"
+	id("io.spring.dependency-management") version "1.0.15.RELEASE"
+	kotlin("jvm") version "1.6.21"
+	kotlin("plugin.spring") version "1.6.21"
+}
+
+group = "br.com.senhavalida"
+version = "1.0.0-api"
+
+repositories {
+	mavenCentral()
+}
+
+dependencies {
+	implementation("com.expediagroup:graphql-kotlin-spring-server:7.0.0-alpha.0")
+	implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
+	implementation("org.jetbrains.kotlin:kotlin-reflect")
+	implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
+	developmentOnly("org.springframework.boot:spring-boot-devtools")
+
+	testImplementation("org.springframework.boot:spring-boot-starter-test")
+}
+
+tasks.withType<KotlinCompile> {
+	kotlinOptions {
+		freeCompilerArgs = listOf("-Xjsr305=strict")
+		jvmTarget = "11"
+	}
+}
+
+tasks.withType<Test> {
+	useJUnitPlatform()
+}
